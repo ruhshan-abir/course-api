29ffba98f5004294ea6cc880a0f62712ea236f87
Ruhshan <ruhshan.abir@bkash.com>
2018-11-16 15:56:40 +0600

    Coure is made non entity

--- src/main/java/com/abir/courseapi/course/Course.java
+++ src/main/java/com/abir/courseapi/course/Course.java
@@ -5,9 +5,8 @@ import com.abir.courseapi.topic.Topic;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+
 public class Course {
-    @Id
     private String id;
     private String name;
     private String description;
